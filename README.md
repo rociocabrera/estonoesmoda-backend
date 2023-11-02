2e5b021ce55e481798540546057eee22d128b1c4
Rocio Cabrera <rocioevelincabrera@gmail.com>
2023-11-02 18:48:18 -0300

    testing cases

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # estonoesmoda-backend
+
+# Esto no es Moda
+
+Welcome to Esto no es Moda! This project is focused on creating an eCommerce for an online clothing store using Javascript.
+
+## Getting Started
+
+1. Clone this repository to your local machine:
+
+```bash
+git clone https://github.com/rociocabrera/estonoesmoda-backend.git
+```
+
+2. Navigate to the project directory:
+
+```bash
+cd estonoesmoda-backend
+```
+
+3. Run the test file
+
+```bash
+node test.js
+```
+
+## License
+
+This project is licensed under the MIT License.
+
+Happy coding! 🛍️👕👖👗
--- productManager.js
+++ productManager.js
@@ -3,6 +3,17 @@ class ProductManager {
     this.products = [];
   }
 
+  validateProduct = ({ title, description, price, thumbnail, code, stock }) => {
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      throw new Error("Missing properties");
+    }
+
+    const existingProduct = this.products.find((product) => product.code === code);
+    if (existingProduct) {
+      throw new Error("Product with code already exists");
+    }
+  };
+
   addProduct = (title, description, price, thumbnail, code, stock) => {
     const newProduct = {
       id: this.products.length + 1,
@@ -13,6 +24,9 @@ class ProductManager {
       code,
       stock,
     };
+
+    this.validateProduct(newProduct);
+
     this.products.push(newProduct);
     return newProduct;
   };
@@ -24,7 +38,7 @@ class ProductManager {
   getProductById(id) {
     const product = this.products.find((product) => product.id === id);
     if (!product) {
-      throw new Error("Not found");
+      throw new Error("Product not found");
     }
     return product;
   }
