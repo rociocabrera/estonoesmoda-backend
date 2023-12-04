fb8f9d8f13187150c31c482301bfa030ffdca649
Rocio Cabrera <rocioevelincabrera@gmail.com>
2023-12-03 23:42:32 -0300

    Remove product remove button and API call

--- src/public/js/realTimeProducts.js
+++ src/public/js/realTimeProducts.js
@@ -7,22 +7,35 @@ socket.on("update-products", (data) => {
     productsList.innerHTML += `
         <li>
         ${product.title}
+        <button onclick="removeProduct(${product.id})">Remove</button>
         </li>
         `;
   });
 });
 
+const removeProduct = (id) => {
+  // call DELETE/api/products/:id endpoint
+  fetch(`/api/products/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+      if (data.status === "error") {
+        alert(data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
 const addProduct = () => {
   const title = document.getElementById("title").value;
-  console.log(title);
   const description = document.getElementById("description").value;
-  console.log(description);
   const price = document.getElementById("price").value;
-  console.log(price);
   const thumbnails = [document.getElementById("thumbnail").value];
-  console.log(thumbnails);
   const code = document.getElementById("code").value;
-  console.log(code);
   const stock = document.getElementById("stock").value;
   const status = document.getElementById("status").checked;
   const category = document.getElementById("category").value;
